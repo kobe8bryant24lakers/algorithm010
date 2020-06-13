35c41dd38b1f845fd18c4a2a1a19cb3a76edfd5a
kobe8bryant24lakers <844040363@qq.com>
2020-06-13 22:07:51 +0800

    作业提交测试

---
+++ Week01/practice/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.algorithm.study.homework.week01;
+
+/**
+ * @ClassName HelloWorld
+ * @Description 测试
+ * @Author 张银兵 ybzhang@yinhai.com
+ * @Date 2020/6/13 22:02
+ * @Version 1.0
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+
+}
